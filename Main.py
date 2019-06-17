05f398ec66c82bec4398c2d9bd4160747f9425ed
Isabella Oliveira <isabellaro@al.insper.edu.br>
2019-06-17 19:13:09 -0300

    Fixing conflicts to merge

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@
 import re
 import sys
 
-reserved = ["PRINT", "BEGIN", "END", "WHILE", "WEND", "IF", "ELSE", "THEN", "AND", "OR", "INPUT", "SUB", "DIM", "AS", "INTEGER", "BOOLEAN", "TRUE", "FALSE", "TYPE", "NOT", "FUNCTION", "CALL"]
-PRINT, BEGIN, END, WHILE, WEND, IF, ELSE, THEN, AND, OR, INPUT, SUB, DIM, AS, INTEGER, BOOLEAN, TRUE, FALSE, TYPE, NOT, FUNCTION, CALL= reserved
+reserved = ["PRINT", "BEGIN", "END", "WHILE", "WEND", "IF", "ELSE", "THEN", "AND", "OR", "INPUT", "SUB", "MAIN", "DIM", "AS", "INTEGER", "BOOLEAN", "TRUE", "FALSE", "TYPE", "NOT"]
+PRINT, BEGIN, END, WHILE, WEND, IF, ELSE, THEN, AND, OR, INPUT, SUB, MAIN, DIM, AS, INTEGER, BOOLEAN, TRUE, FALSE, TYPE, NOT = reserved
 
 class Token:
     def __init__(self, tipo, valor):
@@ -77,10 +77,6 @@ class Tokenizer:
             self.actual = Token("lessthan", "<")
             self.position = self.position + 1
 
-        elif self.origin[self.position] == ',': # se for virgula
-            self.actual = Token("comma", ",")
-            self.position = self.position + 1
-
         elif self.origin[self.position].isalpha():
 
             word = "" #se for palavra vai concatenando, precisa checar se chegou no final
@@ -122,116 +118,30 @@ class Parser:
     @staticmethod
     def Program():
         listafilhos = []
-        while Parser.tokens.actual.type != "eof":
-        # while Parser.tokens.actual.type == SUB or Parser.tokens.actual.type == FUNCTION:
-            if Parser.tokens.actual.type == SUB:
-                listafilhos.append(Parser.SubDec())
-            elif Parser.tokens.actual.type == FUNCTION:
-                listafilhos.append(Parser.FuncDec())
-            while Parser.tokens.actual.type == 'breakline':
-                Parser.tokens.selectNext()
-
-        listafilhos.append(FuncCall("MAIN", []))
-
-        return Statements("Stmts", listafilhos)
-
-    def SubDec():
         if Parser.tokens.actual.type == SUB:
             Parser.tokens.selectNext()
-            if Parser.tokens.actual.type == "identifier":
-                identifier1 = Identifier(Parser.tokens.actual.value, [])
+            if Parser.tokens.actual.type == MAIN:
                 Parser.tokens.selectNext()
-                params = []
                 if Parser.tokens.actual.type == "openpar":
                     Parser.tokens.selectNext()
-                    if Parser.tokens.actual.type == "identifier":
-                        identifier2 = Identifier(Parser.tokens.actual.value, [])
-                        Parser.tokens.selectNext()
-                        if Parser.tokens.actual.type == AS:
-                            Parser.tokens.selectNext()
-                            tipo1 = Parser.parseType()
-                            params.append(VarDec("variável", [identifier2, tipo1]))
-                            while Parser.tokens.actual.type == "comma":
-                                Parser.tokens.selectNext()
-                                if Parser.tokens.actual.type == "identifier":
-                                    identifier3 = Identifier(Parser.tokens.actual.value, [])
-                                    Parser.tokens.selectNext()
-                                    if Parser.tokens.actual.type == AS:
-                                        Parser.tokens.selectNext()
-                                        tipo2 = Parser.parseType()
-                                        params.append(VarDec("variável", [identifier3, tipo2]))
-
-
                     if Parser.tokens.actual.type == "closepar":
                         Parser.tokens.selectNext()
                         if Parser.tokens.actual.type == "breakline":
                             Parser.tokens.selectNext()
-                            stmts = []
-                            while Parser.tokens.actual.type != END:
+                            while Parser.tokens.actual.type != END and Parser.tokens.actual.type != "eof":
                                 node = Parser.Statement()
                                 if node != None:
-                                    stmts.append(node)
-                                if Parser.tokens.actual.type == "breakline":
-                                    Parser.tokens.selectNext()
+                                    listafilhos.append(node)
+                                    if Parser.tokens.actual.type == "breakline":
+                                        Parser.tokens.selectNext()
+                                    ###else raise error?
 
                             if Parser.tokens.actual.type == END:
-                                params.append(Statements("stmts", stmts))### stmts
                                 Parser.tokens.selectNext()
                                 if Parser.tokens.actual.type == SUB:
                                     Parser.tokens.selectNext()
 
-                return SubDec(identifier1.value, params)
-
-    def FuncDec():
-        if Parser.tokens.actual.type == "FUNCTION":
-            Parser.tokens.selectNext()
-            if Parser.tokens.actual.type == "identifier":
-                identifier1 = Identifier(Parser.tokens.actual.value, [])
-                Parser.tokens.selectNext()
-                params = []
-                if Parser.tokens.actual.type == "openpar":
-                    Parser.tokens.selectNext()
-                    if Parser.tokens.actual.type == "identifier":
-                        identifier2 = Identifier(Parser.tokens.actual.value, [])
-                        Parser.tokens.selectNext()
-                        if Parser.tokens.actual.type == AS:
-                            Parser.tokens.selectNext()
-                            tipo1 = Parser.parseType()
-                            params.append(VarDec("variável", [identifier2, tipo1]))
-                            while Parser.tokens.actual.type == "comma":
-                                Parser.tokens.selectNext()
-                                if Parser.tokens.actual.type == "identifier":
-                                    identifier3 = Identifier(Parser.tokens.actual.value, [])
-                                    Parser.tokens.selectNext()
-                                    if Parser.tokens.actual.type == AS:
-                                        Parser.tokens.selectNext()
-                                        tipo2 = Parser.parseType()
-                                        params.append(VarDec("variável", [identifier3, tipo2]))
-
-
-                    if Parser.tokens.actual.type == "closepar":
-                        Parser.tokens.selectNext()
-                        if Parser.tokens.actual.type == AS:
-                            Parser.tokens.selectNext()
-                            tipo3 = Parser.parseType()
-                            if Parser.tokens.actual.type == "breakline":
-                                Parser.tokens.selectNext()
-                                stmts = []
-                                while Parser.tokens.actual.type != END:
-                                    node = Parser.Statement()
-                                    if node != None:
-                                        stmts.append(node)
-                                    if Parser.tokens.actual.type == "breakline":
-                                        Parser.tokens.selectNext()
-
-                                if Parser.tokens.actual.type == END:
-                                    params.append(Statements("stmts", stmts))### stmts
-                                    Parser.tokens.selectNext()
-                                    if Parser.tokens.actual.type == "FUNCTION":
-                                        Parser.tokens.selectNext()
-
-        listona = [tipo3] + params
-        return FuncDec(identifier1.value, listona)
+        return Statements("Statements", listafilhos)
 
     @staticmethod
     def Statement():
@@ -321,23 +231,6 @@ class Parser:
                     tipo = Parser.parseType()
                     return VarDec("variável", [identifier, tipo])
 
-        elif Parser.tokens.actual.type == "CALL":
-            Parser.tokens.selectNext()
-            if Parser.tokens.actual.type == "identifier":
-                identifier = Identifier(Parser.tokens.actual.value, [])
-                Parser.tokens.selectNext()
-                if Parser.tokens.actual.type == "openpar":
-                    Parser.tokens.selectNext()
-                    nodes = []
-                    while Parser.tokens.actual.type != "closepar":
-                        nodes.append(Parser.RelExpression())
-                        if Parser.tokens.actual.type == "comma":
-                            Parser.tokens.selectNext()
-                    
-                    if Parser.tokens.actual.type == "closepar":
-                        Parser.tokens.selectNext()
-                        return FuncCall(identifier.value, nodes)
-
         else:
             return NoOp()
 
@@ -425,21 +318,10 @@ class Parser:
                 left = UnOp(NOT, [left])
         
         elif Parser.tokens.actual.type == "identifier":
-            identifier = Identifier(Parser.tokens.actual.value, [])
+            res = Parser.tokens.actual.value
+            node = Identifier(res, [])
             Parser.tokens.selectNext()
-            nodes = []
-            if Parser.tokens.actual.type == "openpar":
-                Parser.tokens.selectNext()
-                while Parser.tokens.actual.type != "closepar":
-                    nodes.append(Parser.RelExpression())
-                    if Parser.tokens.actual.type == "comma":
-                        Parser.tokens.selectNext()
-                
-                if Parser.tokens.actual.type == "closepar":
-                    Parser.tokens.selectNext()
-                    return FuncCall(identifier.value, nodes)
-            else:         
-                return identifier
+            return node
 
         elif Parser.tokens.actual.type == INPUT:
             Parser.tokens.selectNext()
@@ -469,10 +351,8 @@ class Parser:
         Parser.tokens = Tokenizer(origin, None)
         Parser.tokens.selectNext()
         res = Parser.Program()
-        # if Parser.tokens.actual.type == 'breakline':
-        #     Parser.tokens.selectNext()
-        # if Parser.tokens.actual.type != 'eof':
-        #     raise ValueError('Entrada inválida. Último token não é o EOF. {}: {}'.format(Parser.tokens.actual.type, Parser.tokens.actual.value))
+        if Parser.tokens.actual.type != 'eof':
+            raise ValueError('Entrada inválida. Último token não é o EOF.')
         
         return res
 
@@ -502,24 +382,16 @@ STDIN equ 0
 STDOUT equ 1 
 True equ 1
 False equ 0
-
 segment .data
-
 segment .bss ; variaveis
   res RESB 1
-
-
 section .text 
   global _start
-
 print:  ; subrotina print
-
   PUSH EBP ; guarda o base pointer
   MOV EBP, ESP ; estabelece um novo base pointer
-
   MOV EAX, [EBP+8] ; 1 argumento antes do RET e EBP
   XOR ESI, ESI
-
 print_dec: ; empilha todos os digitos
   MOV EDX, 0
   MOV EBX, 0x000A
@@ -530,55 +402,41 @@ print_dec: ; empilha todos os digitos
   CMP EAX, 0
   JZ print_next ; quando acabar pula
   JMP print_dec
-
 print_next:
   CMP ESI, 0
   JZ print_exit ; quando acabar de imprimir
   DEC ESI
-
   MOV EAX, SYS_WRITE
   MOV EBX, STDOUT
-
   POP ECX
   MOV [res], ECX
   MOV ECX, res
-
   MOV EDX, 1
   INT 0x80
   JMP print_next
-
 print_exit:
   POP EBP
   RET
-
 ; subrotinas if/while
 binop_je:
   JE binop_true
   JMP binop_false
-
 binop_jg:
   JG binop_true
   JMP binop_false
-
 binop_jl:
   JL binop_true
   JMP binop_false
-
 binop_false:
   MOV EBX, False  
   JMP binop_exit
-
 binop_true:
   MOV EBX, True
 binop_exit:
   RET
-
-
 _start :
-
 PUSH EBP ; guarda o base pointer
 MOV EBP, ESP ; estabelece um novo base pointer
-
 """
 
         fim = """; interrupcao de saida
@@ -589,26 +447,16 @@ MOV EBP, ESP ; estabelece um novo base pointer
         with open ("output.nasm", 'w') as file:
                 file.write(inicio + Assembler.stringf + fim)
         
-class SymbolTable(): #agora valor é [valor, tipo]
-    def __init__(self, anterior=None):
+class SymbolTable(): #agora valor é [valor, tipo] ####FALTA TIPO
+    def __init__(self):
         self.table = {}
-        self.anterior = anterior
+        self.shift = 0
 
     def getter(self, chave):
         if chave in self.table.keys():
-            tupla = self.table[chave]
-            #if tupla[0] == None:
-            #    try:
-            #        tupla = tuple(self.anterior.getter(chave))
-            #    except:
-            #        raise ValueError("Falha ao tentar fazer recursão: {}".format(tupla[0]))
-                
+            tupla = tuple(self.table.get(chave))
             return tupla
 
-        elif self.anterior != None:
-            tupla = self.anterior.getter(chave)
-            return tupla
-            
         else:
             raise ValueError("Chave {} não localizada na Tabela de Símbolos".format(chave))
     
@@ -848,7 +696,7 @@ class Assignment(Node):
             Assembler.AddString("MOV [EBP-{}], EBX".format(ST.getter(self.children[0].value)[2]))
             ST.setter(self.children[0].value, tupla[0]) #(nome da variável, value)
         else:
-            raise ValueError ("Variável não compatível com o tipo declarado. {}, {}".format(tipo, tupla[1]))
+            raise ValueError ("Variável não compatível com o tipo declarado.")
 
 class Statements(Node):
     def __init__(self, valor, listafilhos):
@@ -899,54 +747,8 @@ class VarDec(Node):
 
     def Evaluate(self, ST):
         ST.creator(self.children[0].value, self.children[1].Evaluate(ST)[0])
+        Assembler.AddString("PUSH DWORD 0")
 
-class FuncCall(Node):
-    def __init__(self, valor, listafilhos):
-        self.value = valor
-        self.children = listafilhos
-
-    def Evaluate(self, ST):
-        novaST = SymbolTable(ST)
-        funcDec, tipo = novaST.getter(self.value)
-        if tipo == FUNCTION:
-            #Confirma se a qtd de filhos é a mesma tirando tipo (0) e stmts (-1), m = n-2
-            vardecs = funcDec.children[1:-1] #Apenas os vardecs
-            if len(vardecs) == len(self.children):
-                novaST.creator(self.value, funcDec.children[0].Evaluate(novaST)[0])
-                for i in range(len(vardecs)):
-                    if vardecs[i].children[1].value == self.children[i].Evaluate(novaST)[1]: #comparando os tipos
-                        vardecs[i].Evaluate(novaST)
-                        novaST.setter(vardecs[i].children[0].value, self.children[i].Evaluate(novaST)[0])
-            funcDec.children[-1].Evaluate(novaST) ### pega nó statements
-            return novaST.getter(self.value)#Evaluate de todos os statements de acordo com a ST recém criada.
-        
-        elif tipo == SUB:
-            #Confirma se a qtd de filhos é a mesma tirando stmts (-1)
-            vardecs = funcDec.children[:-1] #Apenas os vardecs
-            if len(vardecs) == len(self.children):
-                for i in range(len(vardecs)):
-                    vardecs[i].Evaluate(novaST)
-                    novaST.setter(vardecs[i].children[0].value, self.children[i].Evaluate(novaST)[0])
-            funcDec.children[-1].Evaluate(novaST) ### pega nó statements
-            
-class FuncDec(Node):
-    def __init__(self, valor, listafilhos):
-        self.value = valor
-        self.children = listafilhos
-
-    def Evaluate(self, ST):
-       ST.creator(self.value, FUNCTION)
-       ST.setter(self.value, self)
-
-class SubDec(Node):
-    def __init__(self, valor, listafilhos):
-        self.value = valor
-        self.children = listafilhos
-
-    def Evaluate(self, ST):
-        ST.creator(self.value, SUB)
-        ST.setter(self.value, self)
-        
 def main():
     #try:
         #entrada  = input("Digite o que deseja calcular: ")
@@ -959,6 +761,8 @@ def main():
         res = Parser.run(codigo)
         ST = SymbolTable()
         res.Evaluate(ST)
+        Assembler.WriteFile()
+        
 
     #except Exception as ex:
     #    print(ex)
